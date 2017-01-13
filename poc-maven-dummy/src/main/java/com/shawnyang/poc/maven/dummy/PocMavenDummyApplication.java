61c25fb50b91e97484fd5351ecebaa05dc1801da
SHAWN.SH.YANG <SHAWN.SH.YANG@deltaww.com>
2017-01-13 13:25:29 +0800

    init

---
+++ poc-maven-dummy/src/main/java/com/shawnyang/poc/maven/dummy/PocMavenDummyApplication.java
@@ -0,0 +1,12 @@
+package com.shawnyang.poc.maven.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMavenDummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMavenDummyApplication.class, args);
+	}
+}
