019295ff85b62b84a8eb8dbab696d46414b25fe1
SHAWN.SH.YANG <SHAWN.SH.YANG@deltaww.com>
2017-01-24 15:56:54 +0800

    1

--- poc-maven-install-mongodb/scripts/reinstall.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-call mvn clean assembly:single@zip-scripts antrun:run@reinstall -Dserver.user=root -Dserver.password=P@ssw0rd
-cd %~dp0
--- poc-maven-install-mongodb/src/main/scripts/uninstall-mongodb.sh
+++
@@ -1,4 +0,0 @@
-sudo service mongod stop
-sudo apt-get --assume-yes purge mongodb-org*
-sudo rm -r /var/log/mongodb
-sudo rm -r /var/lib/mongodb
