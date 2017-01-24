329df9e6788e98cad32f1b84cba01dfff917974f
SHAWN.SH.YANG <SHAWN.SH.YANG@deltaww.com>
2017-01-24 21:47:43 +0800

    1

--- poc-maven-antrun/scripts/example.bat
+++ poc-maven-antrun/scripts/example.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 cd ..
-call mvn clean resources:resources deploy assembly:single@zip-scripts antrun:run@upload -Dserver.password=password
+call mvn clean assembly:single@zip-scripts antrun:run@upload antrun:run@run-script -Dscript.run=install-mongodb.sh -Dserver.password=password
 cd %~dp0
--- poc-maven-antrun/src/main/scripts/install-oracle-java7.sh
+++ poc-maven-antrun/src/main/scripts/install-oracle-java7.sh
@@ -8,3 +8,5 @@ sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get install -y oracle-java7-installer
+sudo update-java-alternatives -s java-7-oracle
+java -version
