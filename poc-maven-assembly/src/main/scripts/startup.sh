634f6ef89948a3de8c267c14bbc173283d51e792
Shawn Yang <ocelot.yang.1979@gmail.com>
2017-02-13 19:19:40 +0800

    1

--- poc-maven-assembly/src/main/scripts/startup.sh
+++ poc-maven-assembly/src/main/scripts/startup.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-LIB_PATH=`dirname $0`
-nohup java -jar poc-maven-assembly.jar > /dev/null 2>&1 &
-exit 0
+#!/bin/bashLIB_PATH=`dirname $0`nohup java -jar poc-maven-assembly.jar > /dev/null 2>&1 &exit 0
